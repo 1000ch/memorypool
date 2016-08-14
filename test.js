60d0b4b111ad41846a902b8fc17f4a967085e6e1
1000ch <shogo.sensui@gmail.com>
2016-08-14 22:39:29 +0900

    Brush up tests

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 const test = require('tape');
 const MemoryPool = require('.');
 
-test('MemoryPool is a class', t => {
+test('MemoryPool', t => {
   t.plan(3);
   t.ok(typeof MemoryPool === 'function');
   t.ok(new MemoryPool(Array) instanceof MemoryPool);
   t.throws(MemoryPool);
 });
 
-test('MemoryPool instance behaves memory pool', t => {
-  t.plan(7);
+test('Instance#allocate()', t => {
+  t.plan(3);
 
   let arrayPool = new MemoryPool(Array);
   let array1 = arrayPool.allocate();
@@ -17,7 +17,14 @@ test('MemoryPool instance behaves memory pool', t => {
   t.ok(array1 instanceof Array);
   t.ok(array2 instanceof Array);
   t.ok(arrayPool.size === 0);
+});
 
+test('Instance#free()', t => {
+  t.plan(3);
+
+  let arrayPool = new MemoryPool(Array);
+  let array1 = arrayPool.allocate();
+  let array2 = arrayPool.allocate();
   arrayPool.free(array1);
   arrayPool.free(array2);
   t.ok(arrayPool.size === 2);
@@ -25,6 +32,19 @@ test('MemoryPool instance behaves memory pool', t => {
   let array = arrayPool.allocate();
   t.ok(array instanceof Array);
   t.ok(arrayPool.size === 1);
+});
+
+test('Instance#collect()', t => {
+  t.plan(2);
+
+  let arrayPool = new MemoryPool(Array);
+  let array1 = arrayPool.allocate();
+  let array2 = arrayPool.allocate();
+  let array3 = arrayPool.allocate();
+  arrayPool.free(array1);
+  arrayPool.free(array2);
+  arrayPool.free(array3);
+  t.ok(arrayPool.size === 3);
 
   arrayPool.collect();
   t.ok(arrayPool.size === 0);
